5b80177978297fed5be8e455e711fbf45d8aec55
investxnt-arch <tu@email.com>
2026-02-20 14:30:48 -0500

    Add NextAuth type declarations

---
+++ write_types.py
@@ -0,0 +1,13 @@
+﻿with open('types/next-auth.d.ts', 'w', encoding='utf-8') as f:
+    f.write("""import { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string
+      role: string
+    } & DefaultSession['user']
+  }
+}
+""")
+print('Done')
