5fa7c11eae558b11a791665d5d9b0ceb13648f9e
investxnt-arch <tu@email.com>
2026-02-21 18:31:20 -0500

    Simplify middleware to reduce size

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,4 @@
-import { auth } from './lib/auth'
-import { NextResponse } from 'next/server'
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth
-  const isDashboard = req.nextUrl.pathname.startsWith('/dashboard')
-  
-  if (isDashboard && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-  
-  return NextResponse.next()
-})
+export { auth as default } from './lib/auth'
 
 export const config = {
   matcher: ['/dashboard/:path*']
