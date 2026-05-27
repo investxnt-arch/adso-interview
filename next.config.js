52a8adbbb1abcf56cf9782a9454c84e2113f8449
investxnt-arch <tu@email.com>
2026-05-27 16:06:31 -0500

    Fix next.config.js for ES module

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['viqubhgektepoihutvek.supabase.co'],
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
 }
 
-module.exports = nextConfig
+export default nextConfig
